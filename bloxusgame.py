25988364c608eeffa68f00d5b5a454661009421a
Maciej Gawin <maciej.gawin@gmail.com>
2017-11-18 17:39:28 +0100

    Added more block placement rules.

--- bloxusgame.py
+++ bloxusgame.py
@@ -1,5 +1,4 @@
 import numpy as np
-import io
 
 
 class Game():
@@ -23,6 +22,7 @@ class Game():
 class Player():
     def __init__(self, name, id):
         self.name = name
+        self.id = id
         self.bloxs = []
         self.value = 0
         self._add_blox("shapes.txt")
@@ -31,19 +31,21 @@ class Player():
         with open(filename, "r") as fp:
             element = []
             val = 0
-            lines = fp.readlines()
-            for line in lines:
+            for line in fp:
                 if line.count("0") + line.count("1") + line.count("\n") != len(
                         line):
                     raise RuntimeError("Invalid block shapes file.")
-                if line[0] == "\n" or line.index == len(lines) - 1:
+                if line[0] == "\n":
                     self.bloxs.append(Blox(element, val))
                     self.value += val
                     element = []
                     val = 0
                     continue
                 val += line.count("1")
-                row = [int(i) for i in list(line.strip())]
+                row = [
+                    int(i)
+                    for i in list(line.strip().replace('1', str(self.id)))
+                ]
                 element.append(row)
             if len(element) > 0:
                 self.bloxs.append(Blox(element, val))
@@ -84,7 +86,7 @@ class Blox():
 class Board():
     def __init__(self):
         self.board = np.zeros((13, 13))
-        self.first_move = 2
+        self.moves_count = 0
 
     def show(self):
         print(self.board)
@@ -95,11 +97,38 @@ class Board():
         else:
             for index, val in np.ndenumerate(blox.body):
                 self.board[x + index[0]][y + index[1]] = val
-            if self.first_move > 0:
-                self.first_move -= 1
+            self.moves_count += 1
 
-    def _is_allowed(self, blox, y, x):
+    def _is_allowed(self, blox, x, y):
         ly, lx = np.shape(blox.body)
         if (x + lx > 13) or (y + ly > 13):
             return False
+        if self._overlaps_element(blox, x, y):
+            return False
+        if self._is_illegal_initial_move(blox, x, y):
+            return False
         return True
+
+    def _covers_field(self, blox, x, y, px, py):
+        if (px < x or py < y):
+            return False
+        if (px - x <= len(blox.body)) and (py - y <= len(blox.body[0])):
+            return blox.body[px - x][py - y] > 0
+
+    def _overlaps_element(self, blox, x, y):
+        for index, val in np.ndenumerate(blox.body):
+            if self._covers_field(
+                    blox, x, y, x + index[0], y +
+                    index[1]) and self.board[x + index[0]][y + index[1]] > 0:
+                return True
+        return False
+
+    def _is_illegal_initial_move(self, blox, x, y):
+        print(self.moves_count)
+        blox.show()
+        print(x, y)
+        if self.moves_count < 2:
+            if not (self._covers_field(blox, x, y, 9, 6)
+                    or self._covers_field(blox, x, y, 6, 9)):
+                return True
+        return False
--- game.py
+++ game.py
@@ -3,5 +3,7 @@ import bloxusgame as bg
 game = bg.Game()
 game.show()
 game.playerA.rotate(1)
-game.move(game.playerB.put(1), 0, 9)
+game.move(game.playerB.put(3), 5, 9)
+game.show()
+game.move(game.playerA.put(1), 9, 6)
 game.show()
