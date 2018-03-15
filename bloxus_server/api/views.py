df05cd9db99d0f4959527c145e2e7db372123d8a
Maciej Gawin <maciej.gawin@gmail.com>
2018-03-15 17:53:20 +0100

    Enabled django admin.

--- bloxus_server/api/views.py
+++ bloxus_server/api/views.py
@@ -3,7 +3,6 @@ import bloxus_lib.bloxusgame as bg
 import bloxus_lib.bloxus_strategies as strat
 from .models import Game
 import dill
-
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404
 
@@ -14,12 +13,12 @@ def init(request):
         return HttpResponseBadRequest()
     game = bg.Game(bg.Player("A", 1, strat.random_bvalue_strategy_with_rotates),
                    bg.Player("B", 2, strat.random_bvalue_strategy))
+    player_id = 2
     ser_game = Game()
     ser_game.id = game.id
     ser_game.persisted_game = dill.dumps(game).hex()
-    print(type(ser_game.persisted_game))
     ser_game.save()
-    return JsonResponse({"id": game.id})
+    return JsonResponse({"gid": game.id, "pid": player_id})
 
 
 @csrf_exempt
@@ -28,7 +27,5 @@ def get(request):
         return HttpResponseBadRequest()
     id = request.GET.get('id')
     ser_game = get_object_or_404(Game, pk=id)
-
     game = dill.loads(bytes.fromhex(ser_game.persisted_game))
-
     return JsonResponse({"board": game.board.show()})
