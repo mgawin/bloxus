b4b8361e3a9b231bec7a55c421a9c81857e3aca9
Maciej Gawin <maciej.gawin@gmail.com>
2018-03-19 14:37:36 +0100

    Added support to choose robot opponent while game init action.

--- bloxus_server/api/views.py
+++ bloxus_server/api/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseBadRequest, JsonResponse
 from bloxus_lib import bloxusgame as bg
+from bloxus_lib import bloxus_strategies as strat
 from .models import Game, WaitingGame
 import dill
 from django.views.decorators.csrf import csrf_exempt
@@ -13,32 +14,43 @@ def init(request):
     if not _verify_request_params(request, ["name"], "POST"):
         return HttpResponseBadRequest()
     name = request.POST.get('name')
-    with transaction.atomic():
-        wg = WaitingGame.objects.select_for_update().get(id=1)
-        if wg.gid == "0000":
-            if name is None:
-                name = "Player A"
-            player = bg.Player(name, 1)
-            game = bg.Game(player)
-            wg.gid = game.id
-            gid = game.id
-            wg.save()
-            ser_game = Game()
-            ser_game.id = game.id
-            ser_game.persisted_game = dill.dumps(game).hex()
-            ser_game.save()
-        else:
-            ser_game = get_object_or_404(Game, pk=wg.gid)
-            game = dill.loads(bytes.fromhex(ser_game.persisted_game))
-            if name is None:
-                name = "Player B"
-            player = bg.Player(name, 2)
-            game.add_player(player)
-            gid = wg.gid
-            wg.gid = "0000"
-            wg.save()
-            ser_game.persisted_game = dill.dumps(game).hex()
-            ser_game.save()
+    if request.POST.get('auto') is not None:
+        player = bg.Player(name, 1)
+        robot_player = bg.Player(
+            "Robot", 2, strat.random_bvalue_strategy_with_rotates)
+        game = bg.Game(player, robot_player)
+        ser_game = Game()
+        ser_game.id = game.id
+        gid = game.id
+        ser_game.persisted_game = dill.dumps(game).hex()
+        ser_game.save()
+    else:
+        with transaction.atomic():
+            wg = WaitingGame.objects.select_for_update().get(id=1)
+            if wg.gid == "0000":
+                if name is None:
+                    name = "Player A"
+                player = bg.Player(name, 1)
+                game = bg.Game(player)
+                wg.gid = game.id
+                gid = game.id
+                wg.save()
+                ser_game = Game()
+                ser_game.id = game.id
+                ser_game.persisted_game = dill.dumps(game).hex()
+                ser_game.save()
+            else:
+                ser_game = get_object_or_404(Game, pk=wg.gid)
+                game = dill.loads(bytes.fromhex(ser_game.persisted_game))
+                if name is None:
+                    name = "Player B"
+                player = bg.Player(name, 2)
+                game.add_player(player)
+                gid = wg.gid
+                wg.gid = "0000"
+                wg.save()
+                ser_game.persisted_game = dill.dumps(game).hex()
+                ser_game.save()
     return JsonResponse({"gid": gid, "player": player.input_for_JSON()})
 
 
