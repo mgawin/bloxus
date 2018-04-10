048cb6bdb6d448e11bfb63dd5a43272ad9e97104
Maciej Gawin <maciej.gawin@gmail.com>
2018-04-10 14:48:14 +0200

    Added more tests for block move rules.

--- bloxus_lib/test/test_board.py
+++ bloxus_lib/test/test_board.py
@@ -38,3 +38,59 @@ class TestStringMethods(unittest.TestCase):
         blox = player.get_blox(16)
         print(blox.show())
         self.assertTrue(b._is_allowed(blox, 11, 7))
+
+    def test_board_move_with_rotations(self):
+        b = bg.Board()
+        b.moves_count = 3
+        player = bg.Player('Name', 1)
+        b.board[9][7] = 1
+        b.board[9][8] = 1
+        b.board[9][9] = 2
+        b.board[9][10] = 2
+        b.board[9][11] = 2
+        b.board[9][12] = 2
+        b.board[10][7] = 2
+        b.board[10][8] = 2
+        b.board[10][11] = 2
+        b.board[11][8] = 2
+        b.board[11][10] = 2
+        b.board[12][7] = 2
+        b.board[12][8] = 2
+        b.board[12][10] = 2
+        b.board[12][11] = 2
+        b.board[12][12] = 2
+        b.board[13][12] = 2
+
+        print(b.show())
+        blox = player.get_blox(10)
+        blox.rotate(3)
+        print(blox.show())
+        self.assertTrue(b._is_allowed(blox, 10, 9))
+
+    def test_board_move_allowed4(self):
+        b = bg.Board()
+        b.moves_count = 3
+        player = bg.Player('Name', 1)
+        b.board[6][8] = 1
+        b.board[7][8] = 1
+        b.board[7][9] = 1
+        b.board[7][10] = 1
+        b.board[7][11] = 1
+
+        b.board[3][12] = 2
+        b.board[4][12] = 2
+        b.board[5][12] = 2
+        b.board[6][12] = 2
+        b.board[7][12] = 2
+
+        b.board[8][9] = 2
+        b.board[9][9] = 2
+        b.board[9][10] = 2
+        b.board[9][11] = 2
+        b.board[8][11] = 2
+
+        print(b.show())
+        blox = player.get_blox(16)
+        blox.rotate(1)
+        print(blox.show())
+        self.assertTrue(b._is_allowed(blox, 8, 10))
